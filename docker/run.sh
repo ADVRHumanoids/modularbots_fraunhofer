0c7b3714b81dad1ba0d3dd804f3386cd4f87fdb4
Arturo Laurenzi <arturo.laurenzi@iit.it>
2023-02-02 15:26:49 +0100

    [bugfix] bad flag in docker run

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,6 @@ set -e
 PKG_NAME=ModularBot_6DOF
 
 nvidia-docker run --rm -it \
- -u $(id -u):$(id -g) \
  --env="DISPLAY=$DISPLAY" \
  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
  --name modularbots_fhi \
@@ -13,6 +12,6 @@ nvidia-docker run --rm -it \
  --publish 8888:8888 \
  --volume $(pwd)/docker/setup_catkin_ws.sh:/home/user/setup_catkin_ws.sh \
  --volume $(pwd)/$PKG_NAME:/home/user/catkin_ws/src/$PKG_NAME \
- arturolaurenzi/xbot2:examples \
+ arturolaurenzi/xbot2_focal_base_nvidia:latest \
  bash -c "./setup_catkin_ws.sh; bash"
 
